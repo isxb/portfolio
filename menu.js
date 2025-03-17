03dc20d9101226f6f8e7649a5798c4ee3cc41ad1
Igor Xavier de Brito <145699402+isxb@users.noreply.github.com>
2025-03-17 14:06:19 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,13 @@
 let botaoMenu = document.getElementById('botao-abrir')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
 
 botaoMenu.addEventListener('click', ()=>{
     menu.classList.add('menu-abrir')
